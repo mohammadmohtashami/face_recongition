874c632a74a5cb9c8414b85b6d3345a6999cfdfd
mohammadmohtashami <151357187+mohammadmohtashami@users.noreply.github.com>
2025-11-30 19:53:42 +0330

    add fps on the frame

--- src/video_capture.py
+++ src/video_capture.py
@@ -1,5 +1,6 @@
 import cv2 
 from utils.logger import logger
+import time 
 
 Logger = logger("video_capture")
 
@@ -11,14 +12,21 @@ def stream_capture(SOURCE):
         return
     
     try:
+        prev_frame_time = 0
+        new_frame_time = 0
         while True:
             ret, frame = cap.read()
             if not ret:
                 Logger.error("Failed to grab frame")
                 break
-                
+            new_frame_time = time.time()
+            fps = 1/(new_frame_time - prev_frame_time)
+            FPS = str(int(fps))
+            cv2.putText(frame  , f'FPS : {FPS}'  , (7,70) , cv2.FONT_HERSHEY_SIMPLEX , fontScale=0.8 , color=(100 , 255,0))
+            prev_frame_time = new_frame_time
             yield frame 
             
+                
     finally:
         cap.release()
         cv2.destroyAllWindows()
